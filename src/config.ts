9e1793e7a151ed28fa96953d37f18274624ccaec
muaz234 <ahmedmuaz0152@gmail.com>
2018-10-07 23:53:27 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class Config {
+
+}
+
+export const sender_id = '461551103806';
+export const oneSignalAppId = '3c9cac3e-efe4-4b81-a52a-06d9d08fd29c';
